c18d3b11c271460824926a599e1593f1acee9401
Nate Beck <nbeck@compunet.biz>
2016-08-11 21:23:25 -0700

    adding login handle to spreadsheet and python file

--- Ucs_Install.py
+++ Ucs_Install.py
@@ -1,7 +1,30 @@
 import xlrd
+
 import ucsmsdk
+
 
 file_location = "./UCS.xlsx"
 workbook = xlrd.open_workbook(file_location)
 sheet = workbook.sheet_by_index(1)
+
+# Create Session
+#
+
+
+from ucsmsdk.ucshandle import UcsHandle
+
+handle = UcsHandle( (sheet.cell_value(3,1)),(sheet.cell_value(4,1)),(sheet.cell_value(5,1)))
+
+handle.login()
+
+mo = handle.query_dn("org-root")
+print (mo)
+
+
+#logout
+handle.logout()
+
+
+
+
 
